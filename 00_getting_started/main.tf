12a17bd86e847b2b150bf363bc9e3a4ac4215bdb
Kaung Lin Oo <kaunglinoo.study@gmail.com>
2022-10-20 20:49:31 +0630

    Added input variable

--- 00_getting_started/main.tf
+++ 00_getting_started/main.tf
@@ -12,9 +12,13 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "instance_type" {
+  type = string
+}
+
 resource "aws_instance" "infOps_server" {
   ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "infOps EC2"
