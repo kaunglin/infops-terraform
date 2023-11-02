604decf6a1d20568ceb4f660b41522998aac8baa
Kaung Lin Oo <kaunglinoo.study@gmail.com>
2023-11-03 00:20:00 +0630

    Removed IAM users and added EC2

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+
+}
+
+variable "instance_type" {
+
+}
+
+variable "subnet_id" {
+
+}
+
+variable "region" {
+
+}
+
+variable "access_key" {
+
+}
+
+variable "secret_key" {
+
+}
